22173fc98e4206b2e3ec7850b2941408d4eaad0e
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2022-05-09 11:09:46 +0900

    Delete MYSQL/storage_df/lustre directory
    중복

--- MYSQL/storage_df/lustre/00.storage_work.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /idea_backup/script/default/default_conf
-
-
-	mysql -h${MYSQLHOST} -uroot -p${PASSWORD} IDEA -e "truncate PROJECT_DF_LUSTRE"
-
--- MYSQL/storage_df/lustre/01.storage_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-##############행으로 되어있는 df.txt 파일을 열로 변경하여 storage_data.txt 저장
-source /idea_backup/script/default/default_conf
-
-DIR=/idea_backup/script/MYSQL/storage_df/project_df/lustre
-for FILE in `cat ${DIR}/df.txt`
-do
-	echo $FILE >> ${DIR}//storage_data.txt
-done

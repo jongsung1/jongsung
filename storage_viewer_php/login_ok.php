bc773d6a1989da645b2dfbf11964dd2415736714
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2022-05-09 17:04:20 +0900

    Add files via upload
    로그인 시도 제한 추가

--- storage_viewer_php/login_ok.php
+++ storage_viewer_php/login_ok.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<?php
+<?
 /* 세션 실행 */
 session_start();
 $_SESSION["s_id"]=$_POST["USERID"];
@@ -12,7 +12,7 @@ $PASSWORD = $_POST["PASSWORD"];
 include  "C:/APM_Setup/htdocs/common/dbcon.php";
 
 /* 쿼리 작성 */
-$sql = "select ATTR, USERNAME, USERID, PASSWORD from USER_INFO where USERID='$USERID';";
+$sql = "select ATTR, USERNAME, USERID, PASSWORD,FAIL_COUNT from USER_INFO where USERID='$USERID';";
 //echo $sql;
 
 /* 쿼리 전송(연결 객체) */
@@ -23,7 +23,6 @@ $result = mysqli_query($conn, $sql);
 // mysqli_fetch_array // 필드명
 // mysqli_num_rows // 결과행의 개수
 $num = mysqli_num_rows($result);
-// echo
 
 /* 조건 처리 */
 if(!$num){ // 아이디가 존재하지 않으면
@@ -36,20 +35,33 @@ if(!$num){ // 아이디가 존재하지 않으면
     ";
     exit;
 } else{ // 아이디가 존재하면
-
     // DB에서 사용자 정보 가져오기
     $array = mysqli_fetch_array($result);
+
+    //// FAIL_COUNT 5 이상일때 로그인 lock
+    $FAIL_LIMIT = 5;
+    if($array["FAIL_COUNT"] > $FAIL_LIMIT){ 
+    // 메세지 출력 후 이전 페이지로 이동
+    echo "
+        <script type=\"text/javascript\">
+            alert(\"로그인 실패 $FAIL_LIMIT 회 초과하여 로그인할 수 없습니다.\");
+            history.back();
+        </script>
+    ";
+    exit;
+    }
+
     // $g_idx = $array["idx"];
     // $g_u_name = $array["u_name"];
     // $g_u_id = $array["u_id"];
-
     $g_pwd = $array["PASSWORD"]; // DB추출한 값
-
     $TEMP = $PASSWORD; // 입력 받은 값
     $PASSWORD = hash('sha512', $TEMP); // 넘어온 값 sha512 암호화
-
     // 사용자가 입력한 비밀번호와 DB에 저장된 비밀번호가 일치하지 않는다면
     if($PASSWORD != $g_pwd){
+        $FAIL_COUNT=$array["FAIL_COUNT"]+1;
+        $query = "update USER_INFO set FAIL_COUNT='$FAIL_COUNT' where USERID='$USERID'; ";
+        mysqli_query($conn, $query);
         echo "
             <script type=\"text/javascript\">
                 alert(\"아이디 혹은 비밀번호가 일치하지 않습니다.\");
@@ -57,7 +69,7 @@ if(!$num){ // 아이디가 존재하지 않으면
             </script>
         ";
     exit;
-    } else{ // 비밀번호가 일치한다면
+    }else{ // 비밀번호가 일치한다면
         // 세션 변수 생성
         // $_SESSION["세션변수명"] = 저장할 값;
         
@@ -71,14 +83,12 @@ if(!$num){ // 아이디가 존재하지 않으면
         // 로그인 시 로그 생성
         $query = "insert into LOGIN_HISTORY (USERID, USERIP, LOGINDATE) VALUES ('$USERID','$USERIP','$timestamp')";
         mysqli_query($conn, $query);
-        
-        
-
+        // 로그인 시 FAIL_COUNT 0 으로 초기화
+        $query = "update USER_INFO set FAIL_COUNT=0 where USERID='$USERID'; ";
+        mysqli_query($conn, $query);
         // echo "idx : ".$_SESSION["s_idx"]." / "."NAME : ".$_SESSION["s_name"]." / "."ID : ".$_SESSION["s_id"];
-
         /* DB 연결 종료 */
         #mysqli_close($conn);
-
         /* 페이지 이동 */
         echo "
             <script type=\"text/javascript\">
@@ -86,6 +96,5 @@ if(!$num){ // 아이디가 존재하지 않으면
             </script>
         ";
     };
-
 };
 ?>

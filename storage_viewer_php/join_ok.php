76e36bdef1eb6651ab789177e7dcaa45fc81de0d
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2022-05-10 17:08:09 +0900

    Add files via upload
    페이지 이동 함수 변경

--- storage_viewer_php/join_ok.php
+++ storage_viewer_php/join_ok.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
-
 <?
 	include  "C:/APM_Setup/htdocs/common/dbcon.php";
+	include  "C:/APM_Setup/htdocs/common/funtion.php";
 
 	$userid = $_POST['userid'];
 	$TEMP1 = $_POST['password1']; // 입력 받은 값
@@ -15,73 +15,33 @@
 
 	/// 필수 입력란 체크
 	if(!$TEMP1){
-		echo "
-		<script type=\"text/javascript\">
-			alert(\"패스워드를 입력하세요\");
-			history.back();
-		</script>
-		";
-		exit;
+		go_back("패스워드를 입력하세요");
 	}elseif(!$userid){
-		echo "
-		<script type=\"text/javascript\">
-			alert(\"아이디를 입력하세요\");
-			history.back();
-		</script>
-		";
-		exit;
+		go_back("아이디를 입력하세요");
 	}elseif(!$username){
-		echo "
-		<script type=\"text/javascript\">
-			alert(\"이름을 입력하세요\");
-			history.back();
-		</script>
-		";
-		exit;
+		go_back("이름을 입력하세요");
 	}
 	
 	// 패스워드 일치 확인
 	if($TEMP1 != $TEMP2){
 		// 메세지 출력 후 이전 페이지로 이동
-		echo "
-		<script type=\"text/javascript\">
-			alert(\"패스워드가 일치하지 않습니다\");
-			history.back();
-		</script>
-		";
-		exit;
+		go_back("패스워드가 일치하지 않습니다");
 	}
 
 	// userid 중복 체크
 	$sql = "select USERID from USER_INFO where USERID='$userid';";
 	$result = mysqli_query($conn, $sql);
 	$num = mysqli_num_rows($result);
-
 	
 	if(!$num){ // 아이디가 존재하지 않으면
 		$query1 = "insert into USER_INFO ";
-		$query2 = "(USERID,USERNAME,USERIP,TEAM,ATTR,PASSWORD,OS)";
-		$query3 = "VALUES ('$userid','$username','$USERIP','$team',1,'$userpw1','$os')";
+		$query2 = "(USERID,USERNAME,USERIP,TEAM,ATTR,PASSWORD,OS) ";
+		$query3 = "VALUES ('$userid','$username','$USERIP','$team',1,'$userpw1','$os') ";
 		$query=$query1.$query2.$query3;
 		mysqli_query($conn, $query);
-		echo "
-            <script type=\"text/javascript\">
-                location.href = \"../index_page.php\";
-            </script>
-        ";
-
+		go_target("회원가입이 완료되었습니다.","index_page.php");
 	} else{ // 아이디가 존재하면
 		// 메세지 출력 후 이전 페이지로 이동
-		echo "
-		<script type=\"text/javascript\">
-			alert(\"중복된 아이디 사용\");
-			history.back();
-		</script>
-		";
-		exit;
+		go_back("중복된 아이디 사용");
 	}
-
 ?>
-<meta charset="utf-8" />
-<script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
-

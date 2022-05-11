88fac95a22b807a150cf501d2d054aca02e4a572
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2022-05-11 17:19:09 +0900

    Add files via upload
    콘솔로그 함수 추가 , 콘솔로그 변수로 출력 조절

--- storage_viewer_php/common/funtion.php
+++ storage_viewer_php/common/funtion.php
@@ -1,5 +1,6 @@
 
 <?
+//// alert 후 이전 페이지로 이동
 function go_back($blabla){
     echo "
         <script type=\"text/javascript\">
@@ -9,7 +10,7 @@ function go_back($blabla){
         ";
     exit;
 }
-
+//// alert 후 원하는 페이지로 이동
 function go_target($blabla,$target){
     echo "
         <script type=\"text/javascript\">
@@ -19,5 +20,24 @@ function go_target($blabla,$target){
         ";
     exit;
 }
+/// alert 없이 이동
+function go_no_memtion($target){
+    echo "
+        <script type=\"text/javascript\">
+            location.href = \"$target\";
+        </script>
+        ";
+    exit;
+}
 
+/// f12 콘솔 로그 생성 (Y/N 조절)
+$CONSOLE_LOG = "N"; 
+function console_log( $data ){
+    global $CONSOLE_LOG;
+    if($CONSOLE_LOG == "Y"){
+        echo '<script>';
+        echo 'console.log('. json_encode( $data ) .')';
+        echo '</script>';
+    }
+}
 ?>

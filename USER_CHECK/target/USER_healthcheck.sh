27144ebbd143be3e896eb9fd6304342da9bf1891
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2022-06-23 14:58:59 +0900

    DB 에서 mount_flag 0인 사용자 대상으로 체크로 변경
    check_list.txt 없어도 되도록 변경

--- USER_CHECK/target/USER_healthcheck.sh
+++ USER_CHECK/target/USER_healthcheck.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
 
 source /idea_backup/script/default/default_conf
+#### 사용자 정의 함수 모음
+source /idea_backup/script/HEALTHCHECK/USER_CHECK/USER_healthcheck_config.sh
 
-### renew LIST
-#/idea_backup/script/HEALTHCHECK/USER_CHECK/get_user_info.sh
-LIST=/idea_backup/script/HEALTHCHECK/USER_CHECK/target/check_list.txt
-TABLE=USER_INFO
-PING_COUNT=3
-TIME=20
 MOUNT_CHECK_DIR=/idea_backup/script/USERSET/
 
-NOWTIME=`date +%s`
-update(){
-        SQL=${UPDATE_SQL}
-        mysql -h${MYSQLHOST} -uroot -p${PASSWORD} IDEA -e "${SQL}"
-} 
+#### USER_healthcheck_config.sh 내부 함수
+mysql_sql seq USER_INFO
+get_max_seq seq USER_INFO
+MAXCOUNT=`cat max_seq.txt`
 
-       
-#UPDATE_SQL="update ${TABLE} set FLAG=0,MOUNT_FLAG=0,DATE=$NOWTIME ;"
-#update
-
-for FILE in `cat $LIST`
+for ((var=0 ; var <= ${MAXCOUNT} ; var++));
 do
-	sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${FILE} /idea_backup/script/HEALTHCHECK/USER_CHECK/target/mount_all.sh
 
-	COUNT=`ping -c ${PING_COUNT} ${FILE} | grep Unreachable | wc -l`
-	
-	if [ $COUNT = 0 ] ; then
-		echo -e "${GREEN}${FILE} on${RESET}"
-		MOUNT_DIR_CHECK=`timeout ${TIME}s sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${FILE} /idea_backup/script/HEALTHCHECK/USER_CHECK/DIR_CHECK.sh`
-		if [ ${MOUNT_DIR_CHECK} == "YES" ] ; then
-			MOUNT_COUNT=`timeout ${TIME}s sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${FILE} ${MOUNT_CHECK_DIR}/mount_check.sh |grep not| wc -l`
-			if [ ${MOUNT_COUNT} = 0 ] ; then
-				UPDATE_SQL="update ${TABLE} set FLAG=1,MOUNT_FLAG=1 where USERIP='${FILE}';"
-				update
+	echo -e "${GREEN} ${var} of ${MAXCOUNT} ${RESET}"
+
+	mysql_no_mount_sql ${var} USER_INFO
+	get_ip_by_seq ${var} USER_INFO
+
+	IPCOUNT=`cat userip.txt | wc -l`
+	USER_IP=`cat userip.txt`
+	if [ ${IPCOUNT} -eq 0 ] ; then
+		echo "non user"
+	else
+		sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${USER_IP} /idea_backup/script/HEALTHCHECK/USER_CHECK/target/mount_all.sh
+		COUNT=`ping -c ${PING_COUNT} ${USER_IP} | grep Unreachable | wc -l`
+		if [ $COUNT = 0 ] ; then
+			echo -e "${GREEN} ${USER_IP} on${RESET}"
+			MOUNT_DIR_CHECK=`timeout ${TIME}s sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${USER_IP} /idea_backup/script/HEALTHCHECK/USER_CHECK/DIR_CHECK.sh`
+			if [ ${MOUNT_DIR_CHECK} == "YES" ] ; then
+				MOUNT_COUNT=`timeout ${TIME}s sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${USER_IP} ${MOUNT_CHECK_DIR}/mount_check.sh |grep not| wc -l`
+				if [ ${MOUNT_COUNT} = 0 ] ; then
+					UPDATE_SQL="update ${TABLE} set FLAG=1,MOUNT_FLAG=1 where USERIP='${USER_IP}';"
+					update
+				else
+					UPDATE_SQL="update ${TABLE} set FLAG=1,MOUNT_FLAG=0 where USERIP='${USER_IP}';"
+					update
+				fi
 			else
-				UPDATE_SQL="update ${TABLE} set FLAG=1,MOUNT_FLAG=0 where USERIP='${FILE}';"
+				UPDATE_SQL="update ${TABLE} set FLAG=1,MOUNT_FLAG=0 where USERIP='${USER_IP}';"
 				update
 			fi
+			echo -e "${GREEN}uptime check${RESET}"
+			timeout ${TIME}s sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${USER_IP} /idea_backup/script/HEALTHCHECK/USER_CHECK/get_uptime.sh
+	                timeout ${TIME}s sshpass -p${PASSWORD} ssh -o StrictHostKeyChecking=no root@${USER_IP} /idea_backup/script/HEALTHCHECK/USER_CHECK/SIZE_check.sh
+
 		else
-			UPDATE_SQL="update ${TABLE} set FLAG=1,MOUNT_FLAG=0 where USERIP='${FILE}';"
-			update
+			echo -e "${RED} ${USER_IP} off${RESET}"
 		fi
-	else
-		echo -e "${RED}${FILE} off${RESET}"
 	fi
 done
 

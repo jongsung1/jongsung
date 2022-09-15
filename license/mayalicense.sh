8990373561db9f043b97bbfe39fa7c04e79119a1
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2022-09-15 12:47:29 +0900

    Add files via upload

--- license/mayalicense.sh
+++ license/mayalicense.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+##conf file import
+source /idea_backup/script/default/default_conf
+
+GCOUNT=`cat /etc/group | grep adsklic | wc -l`
+if [ ${GCOUNT} -eq 0 ]; then
+	echo -e "${BOLD} adsklic group 추가${RESET}"
+	groupadd adsklic
+fi
+
+#### maya service start
+echo -e "${BOLD} maya service start ${RESET}"
+systemctl start adsklicensing.service
+
 mkdir -p /var/flexlm/
 
 FILE=/var/flexlm/maya.lic
-
-if [ -z ${FILE} ]; then
+COUNT=`ls ${FILE} | wc -l`
+if [ ${COUNT} -eq 0 ]; then
 	### if FILE name is null
-	echo "SERVER 10.0.99.16 0" >> /var/flexlm/maya.lic
+	echo "SERVER 10.0.99.16 0" > /var/flexlm/maya.lic
 	echo "USE_SERVER" >> /var/flexlm/maya.lic
 fi
 
+#### maya service check
+echo -e "${BOLD} maya service check ${RESET}"
+systemctl status adsklicensing.service
 
